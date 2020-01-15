22f7d24841a2f7ccdb15ab4c1af461aed347e01a
remigiuszk <remigiusz20kaminski@gmail.com>
2020-01-15 21:45:00 +0100

    poprawka

--- src/components/products.js
+++ src/components/products.js
@@ -1,14 +1,10 @@
-// Import statements comes here.
 import React, { Component } from 'react';
 import { Table, Card, Button, CardTitle, CardText, Row, Col, CardImg } from 'reactstrap';
 
-
-// You need to extend the functionality of `Component` to the class created.
 class Product extends Component {
     constructor() {
         super();
 
-// A state can hold anything dynamically. For example here randomVar is any    variable.
 this.state = {
     products: [{ "name": "Horizon Zero Dawn", "img": "1.jpg", "id": 1, "price":   "69.99zl", "rating": "4.6" },
     { "name": "Bloodborne", "img": "2.jpg", "id": 2, "price": "59,99zl",   "rating": "4.8" },
@@ -17,34 +13,28 @@ this.state = {
     { "name": "Wiedzmin 3", "img": "5.jpg", "id": 5, "price": "89zl",   "rating": "4.7" },
     ],
     compare: {
-      // The arr variable stores the id of selected components
+      // tablica przechowujaca id componentow
         arr: []
     }
     
 }
 this.handleClick = this.handleClick.bind(this)
     }
-    // e is the event. For example here the event is `click`. 
-// This variable has some information like: name, id etc. of the component which fired it.
+    // e = event
 handleClick(e) {
-    // Creating a duplicate of arr variable of state
+    // kopiujemy zawartosc tablicy arr to nowej zmiennej
     let temparr = this.state.compare.arr;
-    // e.target.id returns the id of the button which fired the event.
-    //This helps us in identifying the component
+    // e.target.id zwraca id buttona przypisanego do danego obiektu co pozwala nam go zidentyfikowac
     let id = temparr.indexOf(e.target.id)
-    //Checking if the component is already present in the arr variable or    not
+    //walidacja czy dany produkt jest juz na liscie do porownywania
     if (id != -1) {
-      //If present then remove it
         temparr.splice(id, 1)
     }
     else {
-      //else push the id to the temparr
         temparr.push(e.target.id)
     }
-    //setState function helps in changing the state. It can't be done    manually
     this.setState({ compare: { arr: temparr } });
 }
-// This function renders a component
     render() {
         let temparr = this.state.compare.arr
         let temp = []
@@ -54,7 +44,6 @@ handleClick(e) {
             temp.push(x)
         }
 
-        // Whatever is returned is rendered
         return (
             <div>
                 <Row>
